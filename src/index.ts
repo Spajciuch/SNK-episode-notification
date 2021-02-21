f24d2fd093f02028ce7cae6a3e95c4dadefa4c83
spyte <spyte@pm.me>
2021-02-21 15:57:26 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import * as readline from "readline"
+import * as dotenv from "dotenv"
+import fetch from "node-fetch"
+import * as Discord from "discord.js"
+
+dotenv.config()
+
+const client = new Discord.Client()
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const check = function () {
+    rl.question("Podaj numer odcinka do sprawdzenia: ", function (number) {
+        console.log(`Srawdzanie na pojawienie się odcinka ${number}`)
+
+        const fetchInterval = setInterval(function () {
+            fetch(`https://snk.wbijam.pl/czwarta_seria-${number}.html`)
+                .then(res => res.text())
+                .then(body => {
+                    const translated = !body.includes("zwiastun odcinka")
+
+                    if (translated) {
+                        client.emit("episodeAppeared", `https://snk.wbijam.pl/czwarta_seria-${number}.html`)
+                        clearInterval(fetchInterval)
+                    }
+                })
+        }, 1000)
+    })
+}
+
+client.on("ready", () => {
+    console.log(`[discord.js] Discord client logged in`)
+    check()
+})
+
+client.on("episodeAppeared", link => {
+    const spyte = client.users.cache.get("367390191721381890")
+
+    const embed = new Discord.MessageEmbed()
+        .setColor('#00aaff')
+        .setTitle("Shingeki no Kyojin")
+        .setThumbnail("https://media.comicbook.com/2020/12/attack-on-titan-season-4-poster-visual-1247732-1280x0.jpeg")
+        .setDescription(`• Pojawił się nowy odcinek serii **Shingeki no Kyojin**\n• Dostępny [tutaj](${link})`)
+        .setTimestamp()
+    spyte.send(embed)
+})
+
+client.login(process.env.TOKEN)
